447ef3f607a088fe99f3eec66556d7f63610cfbc
Tim van Steenbergen <t.v.steenbergen@gmail.com>
2017-08-03 16:58:46 +0200

    First deploy

---
+++ routes/codes.js
@@ -0,0 +1,88 @@
+var express = require('express');
+
+module.exports = {
+  "2-NL-4015502" : {
+    "Code" : "2-NL-4015502"
+  },
+  "X-NL-40446XX" : {
+    "Code" : "X-NL-40446XX"
+  },
+  "X-NL-40534XX" : {
+    "Code" : "X-NL-40534XX"
+  },
+  "X-NL-40709XX" : {
+    "Code" : "X-NL-40709XX"
+  },
+  "X-NL-41176XX" : {
+    "Code" : "X-NL-41176XX"
+  },
+  "X-NL-41205XX" : {
+    "Code" : "X-NL-41205XX"
+  },
+  "X-NL-41437XX" : {
+    "Code" : "X-NL-41437XX"
+  },
+  "X-NL-42068XX" : {
+    "Code" : "X-NL-42068XX"
+  },
+  "X-NL-42206XX" : {
+    "Code" : "X-NL-42206XX"
+  },
+  "X-NL-42736XX" : {
+    "Code" : "X-NL-42736XX"
+  },
+  "X-NL-42775XX" : {
+    "Code" : "X-NL-42775XX"
+  },
+  "X-NL-43154XX" : {
+    "Code" : "X-NL-43154XX"
+  },
+  "X-NL-43224XX" : {
+    "Code" : "X-NL-43224XX"
+  },
+  "X-NL-43425XX" : {
+    "Code" : "X-NL-43425XX"
+  },
+  "X-NL-43434XX" : {
+    "Code" : "X-NL-43434XX"
+  },
+  "X-NL-43651XX" : {
+    "Code" : "X-NL-43651XX"
+  },
+  "X-NL-43780XX" : {
+    "Code" : "X-NL-43780XX"
+  },
+  "X-NL-43925XX" : {
+    "Code" : "X-NL-43925XX"
+  },
+  "X-NL-41679XX" : {
+    "Code" : "X-NL-41679XX"
+  },
+  "X-NL-42071XX" : {
+    "Code" : "X-NL-42071XX"
+  },
+  "X-NL-42659XX" : {
+    "Code" : "X-NL-42659XX"
+  },
+  "X-NL-42766XX" : {
+    "Code" : "X-NL-42766XX"
+  },
+  "X-NL-43113XX" : {
+    "Code" : "X-NL-43113XX"
+  },
+  "X-NL-43326XX" : {
+    "Code" : "X-NL-43326XX"
+  },
+  "X-NL-43514XX" : {
+    "Code" : "X-NL-43514XX"
+  },
+  "X-NL-43640XX" : {
+    "Code" : "X-NL-43640XX"
+  },
+  "X-NL-43835XX" : {
+    "Code" : "X-NL-43835XX"
+  },
+  "X-NL-43879XX" : {
+    "Code" : "X-NL-43879XX"
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var codes = require('./codes');
+var _ = require('lodash');
+const matcher = require('matcher');
+
 
 /* GET home page. */
 
+
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/zoek/', function(req, res) {
+  res.render('index', { Fout: 'Je moet wel een code invullen.'});
+});
+
+router.get('/zoek/:ei', function(req, res) {
+  var search = req.params.ei;
+  var trimmed = search.substr(5, 5);
+  var eggs = JSON.stringify(codes);
+
+  var status = 'veilig';
+
+  if ( search.includes('nl') != true ) {
+    status = 'veilig';
+  } else if ( eggs.includes(trimmed) == true && trimmed.length == 5 ) {
+    status = 'onveilig';
+  };
+  if ( search.length != 12 ) {
+    status = 'onbekend';
+  };
+  if ( status == 'veilig') {
+    res.render('results', { Title: 'Dit ei is veilig', Results: 'Deze productiecode komt niet voor in de lijst.', Code: req.params.ei } );
+  } else if ( status == 'onveilig') {
+    res.render('results', { Title: 'Je hebt een besmet ei gekocht', Results: 'Deze productiecode komt voor in de lijst met besmette eieren.', Code: req.params.ei } );
+  } else if ( status == 'onbekend') {
+    res.render('results', { Title: 'Er klopt iets niet', Results: 'Dit is geen geldige productiecode.', Code: req.params.ei } );
+  };
+  console.log(status);
+  console.log(req.params.ei);
+
+
+});
+
 
 module.exports = router;
