4a4362ef90762438e27bf0f60c06b5999549cbbe
Tim van Steenbergen <t.v.steenbergen@gmail.com>
2017-08-03 20:10:17 +0200

    Extra codes added

--- routes/codes.js
+++ routes/codes.js
@@ -4,85 +4,178 @@ module.exports = {
   "2-NL-4015502" : {
     "Code" : "2-NL-4015502"
   },
-  "X-NL-40446XX" : {
-    "Code" : "X-NL-40446XX"
+  "X-NL 40004XX" : {
+    "Code" : "X-NL 40004XX"
   },
-  "X-NL-40534XX" : {
-    "Code" : "X-NL-40534XX"
+  "X-NL 40709XX" : {
+    "Code" : "X-NL 40709XX"
   },
-  "X-NL-40709XX" : {
-    "Code" : "X-NL-40709XX"
+  "X-NL 40050XX" : {
+    "Code" : "X-NL 40050XX"
   },
-  "X-NL-41176XX" : {
-    "Code" : "X-NL-41176XX"
+  "X-NL 41176XX" : {
+    "Code" : "X-NL 41176XX"
   },
-  "X-NL-41205XX" : {
-    "Code" : "X-NL-41205XX"
+  "X-NL 40173XX" : {
+    "Code" : "X-NL 40173XX"
   },
-  "X-NL-41437XX" : {
-    "Code" : "X-NL-41437XX"
+  "X-NL 41205XX" : {
+    "Code" : "X-NL 41205XX"
   },
-  "X-NL-42068XX" : {
-    "Code" : "X-NL-42068XX"
+  "X-NL 40309XX" : {
+    "Code" : "X-NL 40309XX"
   },
-  "X-NL-42206XX" : {
-    "Code" : "X-NL-42206XX"
+  "X-NL 41261XX" : {
+    "Code" : "X-NL 41261XX"
   },
-  "X-NL-42736XX" : {
-    "Code" : "X-NL-42736XX"
+  "X-NL 40385XX" : {
+    "Code" : "X-NL 40385XX"
   },
-  "X-NL-42775XX" : {
-    "Code" : "X-NL-42775XX"
+  "X-NL 41435XX" : {
+    "Code" : "X-NL 41435XX"
   },
-  "X-NL-43154XX" : {
-    "Code" : "X-NL-43154XX"
+  "X-NL 40446XX" : {
+    "Code" : "X-NL 40446XX"
   },
-  "X-NL-43224XX" : {
-    "Code" : "X-NL-43224XX"
+  "X-NL 41437XX" : {
+    "Code" : "X-NL 41437XX"
   },
-  "X-NL-43425XX" : {
-    "Code" : "X-NL-43425XX"
+  "X-NL 40494XX" : {
+    "Code" : "X-NL 40494XX"
   },
-  "X-NL-43434XX" : {
-    "Code" : "X-NL-43434XX"
+  "X-NL 41649XX" : {
+    "Code" : "X-NL 41649XX"
   },
-  "X-NL-43651XX" : {
-    "Code" : "X-NL-43651XX"
+  "X-NL 40534XX" : {
+    "Code" : "X-NL 40534XX"
   },
-  "X-NL-43780XX" : {
-    "Code" : "X-NL-43780XX"
+  "X-NL 41679XX" : {
+    "Code" : "X-NL 41679XX"
   },
-  "X-NL-43925XX" : {
-    "Code" : "X-NL-43925XX"
+  "X-NL 40554XX" : {
+    "Code" : "X-NL 40554XX"
   },
-  "X-NL-41679XX" : {
-    "Code" : "X-NL-41679XX"
+  "X-NL 42037XX" : {
+    "Code" : "X-NL 42037XX"
   },
-  "X-NL-42071XX" : {
-    "Code" : "X-NL-42071XX"
+  "X-NL 40567XX" : {
+    "Code" : "X-NL 40567XX"
   },
-  "X-NL-42659XX" : {
-    "Code" : "X-NL-42659XX"
+  "X-NL 42068XX" : {
+    "Code" : "X-NL 42068XX"
   },
-  "X-NL-42766XX" : {
-    "Code" : "X-NL-42766XX"
+  "X-NL 43665XX" : {
+    "Code" : "X-NL 43665XX"
   },
-  "X-NL-43113XX" : {
-    "Code" : "X-NL-43113XX"
+  "X-NL 42071XX" : {
+    "Code" : "X-NL 42071XX"
   },
-  "X-NL-43326XX" : {
-    "Code" : "X-NL-43326XX"
+  "X-NL 42864XX" : {
+    "Code" : "X-NL 42864XX"
   },
-  "X-NL-43514XX" : {
-    "Code" : "X-NL-43514XX"
+  "X-NL 42206XX" : {
+    "Code" : "X-NL 42206XX"
   },
-  "X-NL-43640XX" : {
-    "Code" : "X-NL-43640XX"
+  "X-NL 42894XX" : {
+    "Code" : "X-NL 42894XX"
   },
-  "X-NL-43835XX" : {
-    "Code" : "X-NL-43835XX"
+  "X-NL 42395XX" : {
+    "Code" : "X-NL 42395XX"
   },
-  "X-NL-43879XX" : {
-    "Code" : "X-NL-43879XX"
+  "X-NL 42987XX" : {
+    "Code" : "X-NL 42987XX"
+  },
+  "X-NL 42568XX" : {
+    "Code" : "X-NL 42568XX"
+  },
+  "X-NL 42995XX" : {
+    "Code" : "X-NL 42995XX"
+  },
+  "X-NL 42635XX" : {
+    "Code" : "X-NL 42635XX"
+  },
+  "X-NL 43100XX" : {
+    "Code" : "X-NL 43100XX"
+  },
+  "X-NL 42659XX" : {
+    "Code" : "X-NL 42659XX"
+  },
+  "X-NL 43113XX" : {
+    "Code" : "X-NL 43113XX"
+  },
+  "X-NL 42736XX" : {
+    "Code" : "X-NL 42736XX"
+  },
+  "X-NL 43154XX" : {
+    "Code" : "X-NL 43154XX"
+  },
+  "X-NL 42766XX" : {
+    "Code" : "X-NL 42766XX"
+  },
+  "X-NL 43224XX" : {
+    "Code" : "X-NL 43224XX"
+  },
+  "X-NL 42775XX" : {
+    "Code" : "X-NL 42775XX"
+  },
+  "X-NL 43326XX" : {
+    "Code" : "X-NL 43326XX"
+  },
+  "X-NL 42858XX" : {
+    "Code" : "X-NL 42858XX"
+  },
+  "X-NL 43345XX" : {
+    "Code" : "X-NL 43345XX"
+  },
+  "X-NL 43780XX" : {
+    "Code" : "X-NL 43780XX"
+  },
+  "X-NL 43425XX" : {
+    "Code" : "X-NL 43425XX"
+  },
+  "X-NL 43835XX" : {
+    "Code" : "X-NL 43835XX"
+  },
+  "X-NL 43434XX" : {
+    "Code" : "X-NL 43434XX"
+  },
+  "X-NL 43872XX" : {
+    "Code" : "X-NL 43872XX"
+  },
+  "X-NL 43476XX" : {
+    "Code" : "X-NL 43476XX"
+  },
+  "X-NL 43879XX" : {
+    "Code" : "X-NL 43879XX"
+  },
+  "X-NL 43501XX" : {
+    "Code" : "X-NL 43501XX"
+  },
+  "X-NL 43925XX" : {
+    "Code" : "X-NL 43925XX"
+  },
+  "X-NL 43514XX" : {
+    "Code" : "X-NL 43514XX"
+  },
+  "X-NL 43943XX" : {
+    "Code" : "X-NL 43943XX"
+  },
+  "X-NL 43526XX" : {
+    "Code" : "X-NL 43526XX"
+  },
+  "X-NL 43998XX" : {
+    "Code" : "X-NL 43998XX"
+  },
+  "X-NL 43640XX" : {
+    "Code" : "X-NL 43640XX"
+  },
+  "X-NL 44023XX" : {
+    "Code" : "X-NL 44023XX"
+  },
+  "X-NL 43657XX" : {
+    "Code" : "X-NL 43657XX"
+  },
+  "X-NL 46351XX" : {
+    "Code" : "X-NL 46351XX"
   }
-}
+};
